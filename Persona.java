7178bc702c8d8ece96cd57c254fcbc2b32ea57a8
gaby1702 <condorigabriela1702@gmail.com>
2025-11-17 10:06:20 -0400

    Create Persona.java

---
+++ Persona.java
@@ -0,0 +1,11 @@
+public class Persona {
+    String nombre;
+    int edad;
+    float peso;
+
+    public Persona(String nombre, int edad, float peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+    }
+}
